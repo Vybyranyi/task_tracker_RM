3a04dc8c345c6094b2aa38946932e27c322b2a9f
Vybyranyi <mariankvit@gmail.com>
2026-02-05 18:14:24 +0200

    refactor SideBar component to use SBButtons array for button rendering

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,13 @@
 import styles from "./SideBar.module.scss";
 import SideBarButton from "./SideBarButton";
-import { IoAppsOutline } from "react-icons/io5";
-import { IoBookOutline } from "react-icons/io5";
-import { IoPersonOutline } from "react-icons/io5";
-import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-import { IoSettingsOutline } from "react-icons/io5";
+import SBButtons from "../lib/SBButtons";
 
 const SideBar = () => {
   return (
     <aside className={styles.sideBar}>
-        <SideBarButton icon={<IoAppsOutline />} title="Overview"/>
-        <SideBarButton icon={<IoBookOutline />} title="Task"/>
-        <SideBarButton icon={<IoPersonOutline />} title="Your Team"/>
-        <SideBarButton icon={<IoChatbubbleEllipsesOutline />} title="Message"/>
-        <SideBarButton icon={<IoSettingsOutline />} title="Settings"/>
+      {SBButtons.map((button, index) => (
+        <SideBarButton icon={button.icon} title={button.title} key={index}/>
+      ))}
     </aside>
   )
 }
---
+++ src/lib/SBButtons.jsx
@@ -0,0 +1,15 @@
+import { IoAppsOutline } from "react-icons/io5";
+import { IoBookOutline } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
+
+const SBButtons = [
+  {icon: <IoAppsOutline />, title: "Overview"},
+  {icon: <IoBookOutline />, title: "Task"},
+  {icon: <IoPersonOutline />, title: "Your Team"},
+  {icon: <IoChatbubbleEllipsesOutline />, title: "Message"},
+  {icon: <IoSettingsOutline />, title: "Settings"},
+];
+
+export default SBButtons;   
