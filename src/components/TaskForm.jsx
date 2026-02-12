b53cff06ea4924e4e37c483524deaabde538a295
Vybyranyi <mariankvit@gmail.com>
2026-02-12 18:42:58 +0200

    added styles for task form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -23,59 +23,66 @@ const taskValidationSchema = Yup.object().shape({
 const TaskForm = () => {
   return (
     <div className={styles.formWrapper}>
-        <h2 className="type32">Create Task</h2>
-        <Formik
+      <h2 className="type32">Create Task</h2>
+      <Formik
         initialValues={initialValues}
         validationSchema={taskValidationSchema}
-        onSubmit={values => console.log(values)}
-        >
-        <Form className={styles.form}>
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ isValid, dirty }) => (
+          <Form className={styles.form}>
             <div className={styles.formGroup}>
-                <label htmlFor="title">Task Title</label>
-                <Field
-                    type="text"
-                    name="title"
-                    placeholder="Enter a title..."
-                    className={styles.input}
-                />
-                <ErrorMessage
-                    name="title"
-                    component="div"
-                    className={styles.error}
-                />
+              <label htmlFor="title" className="type14">
+                Task Title
+              </label>
+              <Field
+                type="text"
+                name="title"
+                placeholder="Enter a title..."
+                className={styles.input}
+              />
+              <ErrorMessage
+                name="title"
+                component="div"
+                className={styles.error}
+              />
             </div>
 
             <div className={styles.formGroup}>
-                <Field
-                    as="textarea"
-                    name="description"
-                    placeholder="Enter a description..."
-                    className={styles.textarea}
-                />
-                <ErrorMessage
-                    name="description"
-                    component="div"
-                    className={styles.error}
-                />
+              <label htmlFor="description" className="type14">
+                Task Description
+              </label>
+              <Field
+                as="textarea"
+                name="description"
+                placeholder="Enter a description..."
+                className={styles.textarea}
+              />
+              <ErrorMessage
+                name="description"
+                component="div"
+                className={styles.error}
+              />
             </div>
 
             <div className={styles.formGroup}>
-                <label htmlFor="deadline">Task Deadline</label>
-                <Field
-                    type="date"
-                    name="deadline"
-                    className={styles.input}
-                />
-                <ErrorMessage
-                    name="deadline"
-                    component="div"
-                    className={styles.error}
-                />
+              <label htmlFor="deadline" className="type14">
+                Task Deadline
+              </label>
+              <Field type="date" name="deadline" className={styles.input} />
+              <ErrorMessage
+                name="deadline"
+                component="div"
+                className={styles.error}
+              />
             </div>
 
-            <Button type="submit">Create Task</Button>
-        </Form>
-        </Formik>
+            <Button disabled={!(isValid && dirty)} type="submit">
+              Create Task
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
